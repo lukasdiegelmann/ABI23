0a06fd676ac910d3cf1e8544010ad1d6d8769234
Antimaterie-MHSL <tims.kontakt@gmail.com>
2022-05-04 14:21:01 +0200

    logout

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
     include login.php;
 ?>
+
+
+
+/*
+call on logout button press
+<?php
+    if ($_SESSION['logedin']==1) { session_destroy();
+        header('location:'.$_SERVER['PHP_SELF']);     
+    }
+?>
+*/
