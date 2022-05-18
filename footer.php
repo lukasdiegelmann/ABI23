9dd5a44102c4dc633de570c43a7e2d884ac5af57
Cherry200 <49040607+Cherry200@users.noreply.github.com>
2022-05-18 13:37:41 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
     <body>
     <link href="style.css" type="text/css" rel="stylesheet">
         <div class="footer">
-            <a href="impressum.html"><strong>IMPRESSUM</strong></a> | <a href="datenschutz.html"><strong>DATENSCHUTZ
+            <a href="assets/css/impressum.html"><strong>IMPRESSUM</strong></a> | <a href="assets/css/datenschutz.html"><strong>DATENSCHUTZ
         </div>
     </body>
  </html>
