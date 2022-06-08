f45b4aacc9778f4978b7d1fb816ac85ccd52d38c
heilc <cheil@gmx.de>
2022-06-08 14:59:01 +0200

    Ticker

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <img src="/assets/img/blue-m.jpg" alt="" width="40" height="40">
+        <a href="https://informatik.marianum-fulda.de/abi23" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img src="/assets/imgs/blue-m.jpg" alt="" width="40" height="40">
         </a>
     
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Ticker</a></li>
+            <li><a href="ticker.php" class="nav-link px-2 link-secondary">Ticker</a></li>
             <li><a href="#" class="nav-link px-2 link-dark">Umfragen</a></li>
             <li><a href="#" class="nav-link px-2 link-dark">Bilder</a></li>
         </ul>
