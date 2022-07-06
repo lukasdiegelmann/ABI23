9c033c60a4560143fdba511af27258ab13420859
ℓυкαѕ <lukasdiegelmann@icloud.com>
2022-07-06 14:23:38 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -5,9 +5,12 @@
         </a>
     
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="ticker.php" class="nav-link px-2 link-secondary">Ticker</a></li>
+            <li><a href="ticker.php" class="nav-link px-2 link-dark">Ticker</a></li>
             <li><a href="#" class="nav-link px-2 link-dark">Umfragen</a></li>
             <li><a href="#" class="nav-link px-2 link-dark">Bilder</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Sponsoren</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Vorschläge</a></li>
+            <li><a href="#" class="nav-link px-2 link-dark">Kontakt</a></li>
         </ul>
     
         <div class="col-md-3 text-end">
